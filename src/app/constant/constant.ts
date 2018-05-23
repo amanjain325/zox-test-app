9d07af8b2a46fa6270dce838ddae848a691f6fd6
Aman Jain <amanjain325@gmail.com>
2018-05-23 12:42:29 +0530

    Update constant.ts

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -1,5 +1,5 @@
 export const INSTAGRAM_SECRET_KEYS = {
-    clientId : "3304f85554b3458aa3c7050a3cc4b2d4",
+    clientId : "3304f85554b3458aa3c7050a3cc4b2d4a",
     redirectUri: "http://localhost:3000"
 }
 
@@ -7,7 +7,7 @@ export const URLS = {
     instagramUserDetails: `https://api.instagram.com/v1/users/self/media/recent/`
 }
 
-export const OAUTH_KEY = "6e_HH6slyu5Q01hKxVsztD4HRMo";
+export const OAUTH_KEY = "6e_HH6slyu5Q01hKxVsztD4HRMob";
 
 export const LOCAL_STORAGE_CONSTANTS = {
     access_token: 'access_token'
